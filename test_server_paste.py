4f56fd23f76bdd257f5f524624e313ac757d435f
vmishenev <vad-mishenev@yandex.ru>
2019-11-27 02:36:35 +0300

    fix2

--- test_server_paste.py
+++ test_server_paste.py
@@ -13,9 +13,19 @@ class TestServer(unittest.TestCase):
     def test_notfound(self):
         middleware = []
         testApp = TestApp(app.wsgifunc(*middleware))
-        r = testApp.get('/storage/test')
+        r = testApp.get('/storage/test', status="*")
         self.assertEqual(r.status, 404)
 
+    def test_add(self):
+        middleware = []
+        testApp = TestApp(app.wsgifunc(*middleware))
+        key = 'test'
+        message = 'jhfsdf'
+        obj_data = {'key': key, 'msg': message}
+        r1 = requests.post('/storage/' + key, json=obj_data)
+        r = requests.get('/storage/' + key)
+        self.assertEqual(r1.status, 200)
+        self.assertEqual(r.status, 200)
 
 
 if __name__ == '__main__':
