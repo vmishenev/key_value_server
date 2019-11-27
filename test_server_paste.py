6d7df294864aa37a022a47ae3badc05680bca79c
vmishenev <vad-mishenev@yandex.ru>
2019-11-27 03:30:12 +0300

    testAll

--- test_server_paste.py
+++ test_server_paste.py
@@ -28,6 +28,21 @@ class TestServer(unittest.TestCase):
         self.assertEqual(r.status, 200)
         self.assertEqual(json.loads(r.body.decode('utf-8'))['message'], message)
 
+    def test_delete(self):
+        middleware = []
+        testApp = TestApp(app.wsgifunc(*middleware))
+        key = 'test3'
+        message = 'jhfsdf'
+        obj_data = {'key': key, 'message': message}
+        r1 = testApp.post('/storage/' + key, params=json.dumps(obj_data))
+        r = testApp.get('/storage/' + key)
+        self.assertEqual(r1.status, 200)
+        self.assertEqual(r.status, 200)
+        self.assertEqual(json.loads(r.body.decode('utf-8'))['message'], message)
+        r3 = testApp.delete('/storage/' + key)
+        r4 = testApp.get('/storage/'+ key, status="*")
+        self.assertEqual(r.status, 404)
+
 if __name__ == '__main__':
     unittest.main()
 
